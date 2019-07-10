21f6c9dd3bb44824b0022f03d5cb25ea0ef4837b
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 00:14:52 +0000

    Commit from Transposit IDE

--- src/accepttos.js
+++ src/accepttos.js
@@ -1,5 +1,5 @@
 ({ http_event, db }) => {
-  let body = JSON.parse(http_event.body);
+  let body = JSON.parse(http_event.parsed_body.payload);
   //api.run('this.create_record', {id: body.user.id})
   return body;{ status_code: 200 };
 }

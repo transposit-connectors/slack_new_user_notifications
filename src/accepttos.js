bf3a33bac717a8b88b2fa213363fb9ed1911b314
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 00:50:22 +0000

    Commit from Transposit IDE

--- src/accepttos.js
+++ src/accepttos.js
@@ -1,5 +1,12 @@
 ({ http_event, db }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
-  api.run('this.create_record', {id: body.user.id})
-  return body;{ status_code: 200 };
+  let records = api.run('this.get_records');
+  // Find the right record id in airtable
+  let i = 0, record = records[0].id;
+  while (records[i].fields.slackId != body.user.id) {
+    i += 1;
+    record = records[i].id;
+  }
+  api.run('this.update_record', {record: record, id: body.user.id})
+  return { status_code: 200 };
 }
--- src/reminder.js
+++ src/reminder.js
@@ -1,6 +1,12 @@
 ( params ) => {
-  let approved = api.run('this.get_records');
-  for (let i = 0; i < approved.length; i++) {
-    
+  let raw_approved = api.run('this.get_records');
+  let approved = [];
+  for (let i = 0; i < raw_approved.length; i++) {
+    if (raw_approved[i].slackId && !(approved.includes(raw_approved[i].slackId))) {
+      approved.push(raw_approved[i].slackId);
+    }
   }
+  if ()
+  
+  return approved;//.includes('ULBCAQ66BH');
 }

0a027173ea2a729fec9b3d56b1fcc62319f6333c
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-10 00:53:30 +0000

    Commit from Transposit IDE

--- src/reminder.js
+++ src/reminder.js
@@ -2,11 +2,11 @@
   let raw_approved = api.run('this.get_records');
   let approved = [];
   for (let i = 0; i < raw_approved.length; i++) {
-    if (raw_approved[i].slackId && !(approved.includes(raw_approved[i].slackId))) {
-      approved.push(raw_approved[i].slackId);
+    if (raw_approved[i].fields.slackId && !(approved.includes(raw_approved[i].fields.slackId))) {
+      approved.push(raw_approved[i].fields.slackId);
     }
   }
-  if ()
+  //if ()
   
   return approved;//.includes('ULBCAQ66BH');
 }

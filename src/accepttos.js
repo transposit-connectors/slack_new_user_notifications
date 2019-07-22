c4192f20043616173e257f45f6e13c08e220c9c2
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-22 17:32:05 +0000

    Commit from Transposit IDE

--- src/accepttos.js
+++ src/accepttos.js
@@ -1,13 +1,5 @@
 ({ http_event, db }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
-  let records = stash.get("db");
-  // Find the right record id in airtable
-  let i = 0, record = records[0][0];
-  while (records[i][0] != body.user.id) {
-    i += 1;
-    record = records[i].id;
-  }
-  records[i][1] = true;
-  stash.put("db", records);
+  stash.put(body.user.id, true);
   return { status_code: 200 };
 }

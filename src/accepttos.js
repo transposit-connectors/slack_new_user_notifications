fcde6acc75136bd3a91d96c3dcea1773c056e108
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-22 18:02:46 +0000

    Commit from Transposit IDE

--- src/accepttos.js
+++ src/accepttos.js
@@ -1,5 +1,6 @@
 ({ http_event, db }) => {
   let body = JSON.parse(http_event.parsed_body.payload);
+  //
   stash.put(body.user.id, true);
   return { status_code: 200 };
 }
--- src/webhook.js
+++ src/webhook.js
@@ -10,6 +10,15 @@
   }
   
   if (!http_event.headers['X-Slack-Retry-Num']) {
+    let keys = [];
+    if (stash.get("keys") == null)) {
+      stash.put("keys", keys);
+    }
+    else {
+      keys = stash.get("keys");
+      keys.push(body.event.user.id);
+    }
+    
     stash.put(body.event.user.id, false);
     api.run('this.post_tos', {userid: body.event.user.id, message: 'Welcome to the team! We\'re glad you\'re here.'});
   }

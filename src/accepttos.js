f3db92c1e2d4e3bcd2875818f59cc76ea31cd241
Griffin Solot-Kehl <griffin@transposit.com>
2019-09-26 17:38:53 +0000

    Commit from Transposit IDE

--- src/accepttos.js
+++ src/accepttos.js
@@ -1,5 +1,7 @@
 ({ http_event }) => {
-  let body = JSON.parse(http_event.parsed_body.payload);
-  stash.put(body.user.id, true);
+  setImmediate(() => {
+    let body = JSON.parse(http_event.parsed_body.payload);
+    stash.put(body.user.id, true);
+  });
   return { status_code: 200 };
 }
